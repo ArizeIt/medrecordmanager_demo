1f21e7a964e0460d6a9277e731418c266b8fb9cc
Danny Li <xingkang.li@boeing.com>
2019-02-19 15:08:55 -0500

    add

---
+++ MedRecordManager/Models/ClinicRecord/ClinicVm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedRecordManager.Models.ClinicRecord
+{
+    public class ClinicVm
+    {
+        public SearchInputs Input { get; set; }
+
+        public string PVClinicName { get; set; }
+
+        public string AmdFircilityName { get; set; }
+
+        public List<int > OfficeKeys { get; set; }
+    }
+}
