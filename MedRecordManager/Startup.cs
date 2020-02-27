906cf26ff7b5447fbc0d445cfbfb9bb65011e90c
Danny Li <xingkang.li@boeing.com>
2020-02-27 15:09:08 -0500

    add roles

--- MedRecordManager/Startup.cs
+++ MedRecordManager/Startup.cs
@@ -42,7 +42,7 @@ namespace MedRecordManager
                  options.UseSqlServer(
                  Configuration.GetConnectionString("accountConnection")));
 
-            services.AddDefaultIdentity<IdentityUser>()
+            services.AddDefaultIdentity<IdentityUser>().AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
            
@@ -101,7 +101,6 @@ namespace MedRecordManager
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseSession();
-
             app.UseAuthentication();
           
             app.UseMvc(routes =>
@@ -116,46 +115,46 @@ namespace MedRecordManager
             });
         }
 
-        private void CreateRoles(IServiceProvider serviceProvider)
-        {
+        //private void CreateRoles(IServiceProvider serviceProvider)
+        //{
 
-            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-            Task<IdentityResult> roleResult;
-            string email = "someone@somewhere.com";
+        //    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        //    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        //    Task<IdentityResult> roleResult;
+        //    string email = "Danny.x.li@gmail.com";
 
-            //Check that there is an Administrator role and create if not
-            Task<bool> hasAdminRole = roleManager.RoleExistsAsync("Administrator");
-            hasAdminRole.Wait();
+        //    //Check that there is an Administrator role and create if not
+        //    Task<bool> hasAdminRole = roleManager.RoleExistsAsync("Administrator");
+        //    hasAdminRole.Wait();
 
-            if (!hasAdminRole.Result)
-            {
-                roleResult = roleManager.CreateAsync(new IdentityRole("Administrator"));
-                roleResult.Wait();
-            }
+        //    if (!hasAdminRole.Result)
+        //    {
+        //        roleResult = roleManager.CreateAsync(new IdentityRole("Administrator"));
+        //        roleResult.Wait();
+        //    }
 
-            //Check if the admin user exists and create it if not
-            //Add to the Administrator role
+        //    //Check if the admin user exists and create it if not
+        //    //Add to the Administrator role
 
-            Task<ApplicationUser> testUser = userManager.FindByEmailAsync(email);
-            testUser.Wait();
+        //    Task<ApplicationUser> testUser = userManager.FindByEmailAsync(email);
+        //    testUser.Wait();
 
-            if (testUser.Result == null)
-            {
-                ApplicationUser administrator = new ApplicationUser();
-                administrator.Email = email;
-                administrator.UserName = email;
+        //    if (testUser.Result == null)
+        //    {
+        //        ApplicationUser administrator = new ApplicationUser();
+        //        administrator.Email = email;
+        //        administrator.UserName = email;
 
-                Task<IdentityResult> newUser = userManager.CreateAsync(administrator, "Danny.x.li@gmail.com");
-                newUser.Wait();
+        //        Task<IdentityResult> newUser = userManager.CreateAsync(administrator, "Danny.x.li@gmail.com");
+        //        newUser.Wait();
 
-                if (newUser.Result.Succeeded)
-                {
-                    Task<IdentityResult> newUserRole = userManager.AddToRoleAsync(administrator, "Administrator");
-                    newUserRole.Wait();
-                }
-            }
+        //        if (newUser.Result.Succeeded)
+        //        {
+        //            Task<IdentityResult> newUserRole = userManager.AddToRoleAsync(administrator, "Administrator");
+        //            newUserRole.Wait();
+        //        }
+        //    }
 
-        }
+        //}
     }
 }
